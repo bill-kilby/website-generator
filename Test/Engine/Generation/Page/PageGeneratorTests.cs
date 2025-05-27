7ce2ecb97580f19a8dd2fadc566ac0c067246ee5
bill-kilby <billo.kilby@yahoo.co.uk>
2025-05-27 19:16:45 +0100

    PageGeneratorTests added.

--- Test/Engine/Generation/Page/PageGeneratorTests.cs
+++ Test/Engine/Generation/Page/PageGeneratorTests.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NSubstitute;
+using Test.Engine.Generation.Sections.Mocks;
+using website_generator.Domain.Generation.Sections;
+using website_generator.Engine.Generation.Page;
 
 namespace Test.Engine.Generation.Page
 {
@@ -11,13 +10,33 @@ namespace Test.Engine.Generation.Page
         [Fact]
         public void WhenGeneratingPage_GivenValidSetup_ThenPageGenerated()
         {
-            Assert.Fail();
+            // Assemble
+            var sectionFactoryVerifier = Substitute.For<ISectionFactoryVerifier>();
+            var sections = new List<ISectionFactory>()
+            {
+                new SectionFactoryMock(sectionFactoryVerifier)
+            };
+            var generator = new PageGenerator(sections);
+
+            // Act
+            var result = generator.Generate();
+
+            // Assert
+            Assert.NotNull(result);
         }
 
         [Fact]
         public void WhenGeneratingPage_GivenNoSections_ThenEmptyPageGenerated()
         {
-            Assert.Fail();
+            // Assemble
+            var sections = new List<ISectionFactory>();
+            var generator = new PageGenerator(sections);
+
+            // Act
+            var result = generator.Generate();
+
+            // Assert
+            Assert.Equal("", result);
         }
     }
 }
